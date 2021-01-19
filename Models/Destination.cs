083dbea7da3bfd2a165ac6fa384d75c2d36022e2
Zahnen Garner <zahnen@gmail.com>
2021-01-19 10:34:07 -0800

    add Destination model logic

---
+++ Models/Destination.cs
@@ -0,0 +1,11 @@
+namespace TravelApi.Models
+{
+  public class Destination
+  {
+    public int DestinationId { get; set; }
+    public string CityName { get; set; }
+    public string Country { get; set; }
+    public string Review { get; set; }
+    public int Rating { get; set; }
+  }
+}
