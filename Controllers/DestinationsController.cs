e1debbe9d883ba02cf018ceeea013c722b44924a
Zahnen Garner <zahnen@gmail.com>
2021-01-19 11:07:21 -0800

    add DestinationsController with get and post routes

---
+++ Controllers/DestinationsController.cs
@@ -0,0 +1,34 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TravelApi.Models;
+
+namespace TravelApi.Controllers
+{
+  [Route("api/[controller]")]
+  [ApiController]
+  public class DestinationsController : ControllerBase
+  {
+    private TravelApiContext _db;
+
+    public DestinationsController(TravelApiContext db)
+    {
+      _db = db;
+    }
+
+    // GET api/destinations
+    [HttpGet]
+    public ActionResult<IEnumerable<Destination>> Get()
+    {
+      return _db.Destinations.ToList();
+    }
+
+    // POST api/destinations
+    [HttpPost]
+    public void Post([FromBody] Destination destination)
+    {
+      _db.Destinations.Add(destination);
+      _db.SaveChanges();
+    }
+  }
+}
