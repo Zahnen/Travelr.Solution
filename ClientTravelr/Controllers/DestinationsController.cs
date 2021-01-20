6a5b29e1ca65dfc368b108588b56871991da8e7a
Zahnen Garner <zahnen@gmail.com>
2021-01-20 15:27:25 -0800

    add CRUD functionality to DestinationsController, add corresponding Views
    Co-authored-by: Kaila Sprague McRae <kaila.sprague@icloud.com>

--- ClientTravelr/Controllers/DestinationsController.cs
+++ ClientTravelr/Controllers/DestinationsController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using System.Linq;
 
 namespace ClientTravelr.Controllers
@@ -19,6 +20,40 @@ namespace ClientTravelr.Controllers
       {
         var thisDestination = Destination.GetDetails(id);
         return View(thisDestination);
+      }
+
+      public IActionResult Create()
+      {
+        return View();
+      }
+
+      [HttpPost]
+      public IActionResult Create(Destination destination)
+      {
+        Destination.Post(destination);
+        var timer = Task.Delay(100); //DO NOT KEEP THIS HERE
+        timer.Wait();
+        return RedirectToAction("Index");
+      }
+
+      public IActionResult Edit(int id)
+      {
+        var thisDestination = Destination.GetDetails(id);
+        return View(thisDestination);
+      }
+
+      [HttpPost]
+      public IActionResult Edit(Destination destination)
+      {
+        Destination.Put(destination);
+        return RedirectToAction("Details", new { id = destination.DestinationId });
+      }
+
+      [HttpPost]
+      public IActionResult Delete(int id)
+      {
+        Destination.Delete(id);
+        return RedirectToAction("Index");
       }
     }
 }
